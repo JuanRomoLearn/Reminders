d080f50661989899d630ec9e1451ce46edeea13d
Juan Romo <juanromolearn@gmail.com>
2017-11-18 16:48:27 -0600

    Apache license version 2.0 added

--- app/src/main/java/io/romo/reminders/data/RemindersDbHelper.java
+++ app/src/main/java/io/romo/reminders/data/RemindersDbHelper.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2017 Juan Romo
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.romo.reminders.data;
 
 import android.content.Context;
@@ -24,7 +40,7 @@ public class RemindersDbHelper extends SQLiteOpenHelper {
                 ReminderEntry.COLUMN_DESCRIPTION + " TEXT NOT NULL DEFAULT ''," +
                 ReminderEntry.COLUMN_PRIORITY + " TEXT NOT NULL," +
                 ReminderEntry.COLUMN_COMPLETED + " INTEGER NOT NULL," +
-                ReminderEntry.COLUMN_CREATION_DATE + " INTEGER NOT NULL" +
+                ReminderEntry.COLUMN_CREATION_DATE + " TIMESTAMP NOT NULL" +
                 ")"
         );
     }
